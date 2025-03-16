efb3d7c010221f08aa3175fc79d304b1fba4369f
Yi Yang @ Anteros <ahyangyi@gmail.com>
2025-03-16 23:40:18 +0800

    Real garden

--- station/stations/dovemere_2018_lib/demos/plaza/west_plaza_realgardens.py
+++ station/stations/dovemere_2018_lib/demos/plaza/west_plaza_realgardens.py
@@ -41,6 +41,7 @@ yard = real(576, 129).lower_tile()
 path = real(560, 129).lower_tile()
 pavilion_L = real(626, 157).lower_tile()
 pavilion_R = real(624, 156).lower_tile()
+thick_tee = real(203, 138).lower_tile()
 tee_L = real(568, 208).lower_tile()
 tee_R = real(570, 208).lower_tile()
 big_tee = real(582, 188).lower_tile()
@@ -61,7 +62,7 @@ edge_bottom = real(617, 241).lower_tile()
 
 
 west_square = [
-    [ground, grassy, ground, ground, ground, grassy, ground],
+    [ground, grassy, ground, thick_tee, ground, grassy, ground],
     [zag_L, edge_bottom, corner_TL, path, corner_TR, edge_bottom, zag_R],
     [corner_L, big_tee, zig_L, crossroads, zig_R, big_tee, corner_R],
     [pavilion_L, tee_L, grassy, path, grassy, tee_R, pavilion_R],
