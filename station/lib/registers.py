61fabb0e35f30aedcdde2b5a657b210c0cd87d39
Yi Yang @ Anteros <ahyangyi@gmail.com>
2025-04-29 23:10:54 +0800

    Fix: assign registers 0x10/0x11 to buffer stops

--- station/lib/registers.py
+++ station/lib/registers.py
@@ -3,14 +3,14 @@ from agrf.lib.building.registers import Registers as AGRFRegisters, code as agrf
 
 
 class Registers(AGRFRegisters):
-    RAIL_CONTINUATION_S = grf.Temp(0x20)
-    RAIL_CONTINUATION_N = grf.Temp(0x21)
+    RAIL_CONTINUATION_S = grf.Temp(0x10)
+    RAIL_CONTINUATION_N = grf.Temp(0x11)
 
 
 code = (
     agrf_code
     + """
-TEMP[0x20] = (rail_continuation & 0x1) == 0
-TEMP[0x21] = (rail_continuation & 0x2) == 0
+TEMP[0x10] = (rail_continuation & 0x1) == 0
+TEMP[0x11] = (rail_continuation & 0x2) == 0
 """
 )
