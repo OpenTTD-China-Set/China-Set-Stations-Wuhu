OpenTTD-China-Set/China-Set-Stations-Wuhu

commit 75f538f510eaee102bf15309fbddb528b5e9b438

Lint: eliminate unused imports and f-strings

station/lib/docgen.py
@@ -139,12 +139,12 @@ changelog""",
                 print(item, file=f)
             if len(demo_toc) > 0:
                 print(f"{metastation_label}_demo", file=f)
-            print(f"```\n", file=f)
+            print("```\n", file=f)
 
         if len(demo_toc) > 0:
             with open(os.path.join(prefix, f"{metastation_label}_demo.md"), "w") as f:
                 print(
-                    f"""# Demos
+                    """# Demos
 
 ```{{toctree}}
 :maxdepth: 2""",
@@ -152,4 +152,4 @@ changelog""",
                 )
                 for item in demo_toc:
                     print(item, file=f)
-                print(f"```\n", file=f)
+                print("```\n", file=f)

station/stations/dovemere_2018_lib/flexible_stations/semitraversable.py
@@ -2,7 +2,7 @@ import grf
 from station.lib import AStation, StationTileSwitch, make_vertical_switch
 from .. import common_cb, common_code
 from ..layouts import named_tiles, layouts
-from .common import determine_platform_odd, determine_platform_even, make_front_row, make_demo, make_row
+from .common import determine_platform_odd, determine_platform_even, make_demo
 from .traversable import cb14_0, cb14_2, cb14_4, cb14_6, fill_odd
 from station.stations.platforms import platform_classes, shelter_classes
 from station.lib.parameters import parameter_list

station/stations/dovemere_2018_lib/flexible_stations/side_third.py
@@ -5,7 +5,6 @@ from .. import common_cb, common_code
 from .common import make_demo, horizontal_layout
 from station.stations.platforms import platform_classes, shelter_classes
 from station.lib.parameters import parameter_list
-from ..foundation import named_foundations
 
 named_tiles.globalize()
 

commit f7756eddcb0d8b4a17711186228f84c9932b4aff

Fix: double braces in rst file.
Bug was introduced in commit 75f538f5

station/lib/docgen.py
@@ -131,7 +131,7 @@ changelog""",
             print(
                 f"""# {translation}
 
-```{{toctree}}
+```{toctree}
 :maxdepth: 2""",
                 file=f,
             )