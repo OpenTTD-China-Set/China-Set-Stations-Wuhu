b3b90ac3af288db507cea679c7fc1f79f79851cf
Yi Yang @ Asterodeia <ahyangyi@gmail.com>
2025-08-17 01:45:49 +0800

    .......

--- station/stations/platform_lib/demos.py
+++ station/stations/platform_lib/demos.py
@@ -6,7 +6,7 @@ two_side_tiles.globalize()
 concourse_tiles.globalize()
 
 demos = [
-    Demo([[cns_concrete], [cns_concrete_d], [cns_concrete.T]], "Platform"),
+    Demo([[cns_concrete], [concourse_concrete_d], [cns_concrete.T]], "Platform"),
     Demo([[cns_concrete_side], [cns_concrete_d], [cns_concrete_side.T]], "Platform with concrete grounds"),
     Demo([[cns_concrete_shelter_1], [cns_concrete_shelter_1_d], [cns_concrete_shelter_1.T]], "Platform with shelter"),
 ]
