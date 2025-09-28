39a838a55f5190e95cb37e3e856e202fc168be92
Yi Yang @ Asterodeia <ahyangyi@gmail.com>
2025-09-29 04:29:35 +0800

    ..

--- station/lib/registers.py
+++ station/lib/registers.py
@@ -5,6 +5,8 @@ from agrf.lib.building.registers import Registers as AGRFRegisters, code as agrf
 class Registers(AGRFRegisters):
     RAIL_CONTINUATION_S = grf.Temp(0x10)
     RAIL_CONTINUATION_N = grf.Temp(0x11)
+    NEEDS_ESCALATOR_S = grf.Temp(0x12)
+    NEEDS_ESCALATOR_N = grf.Temp(0x13)
 
 
 code = (
@@ -12,5 +14,7 @@ code = (
     + """
 TEMP[0x10] = (rail_continuation & 0x1) == 0
 TEMP[0x11] = (rail_continuation & 0x2) == 0
+TEMP[0x12] = var(0x68, param=0x10, shift=0, and=0x300) == 0x100
+TEMP[0x13] = var(0x68, param=0xF0, shift=0, and=0x300) == 0x100
 """
 )
