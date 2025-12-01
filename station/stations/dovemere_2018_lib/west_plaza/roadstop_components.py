b9b96a778c0a9877ea6a33567f849ff951048f82
Yi Yang @ Asterodeia <ahyangyi@gmail.com>
2025-12-01 22:33:02 +0800

    .

--- station/stations/dovemere_2018_lib/west_plaza/roadstop_components.py
+++ station/stations/dovemere_2018_lib/west_plaza/roadstop_components.py
@@ -98,7 +98,7 @@ def make_components():
     make_component("road_stop", "stair_extender", BuildingFull, (8, 8, TOTAL_HEIGHT), (8, 6, 0), joggle=JOGGLE_AMOUNT)
     make_component("road_stop", "stair_extender_shallow", BuildingFull, (8, 8, TOTAL_HEIGHT), (8, 6, 0), joggle=0)
     make_component(
-        "road_stop", "stair_extender_onesided", BuildingFull, (8, 8, PLATFORM_HEIGHT), (8, 6, 0), joggle=JOGGLE_AMOUNT
+        "road_stop", "stair_extender_onesided", BuildingFull, (8, 8, TOTAL_HEIGHT), (8, 6, 0), joggle=JOGGLE_AMOUNT
     )
 
     # Underground Entrance
