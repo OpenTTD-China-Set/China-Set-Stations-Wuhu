d99618324c83d1458e4845c74d2cb53fa7885b2b
Yi Yang @ Asterodeia <ahyangyi@gmail.com>
2025-12-10 03:35:54 +0800

    Refactor: use the new demo altitude API

--- station/stations/dovemere_2018_lib/demos/plaza/west_plaza.py
+++ station/stations/dovemere_2018_lib/demos/plaza/west_plaza.py
@@ -5,29 +5,29 @@ from station.stations.dovemere_2018_lib.flexible_stations import semitraversable
 from station.stations.dovemere_2018_lib.roadstops import named_layouts as roadstop_layouts
 from station.stations.dovemere_2018_lib.objects import named_layouts as object_layouts
 from station.stations.dovemere_2018_lib.layouts import globalize_all
-from station.stations.misc import slope_2, road_ground_vanilla_layout
+from station.stations.misc import default, road_ground_vanilla_layout
 from ..utils import h_merge
 
 globalize_all(platform_class="concrete", shelter_class="shelter_2")
 roadstop_layouts.globalize()
 object_layouts.globalize()
 
-station = h_merge(
-    [[[cns], [slope_2.lower_tile()]], semitraversable.demo_1(5, 7)[5:], [[cns], [slope_2.lower_tile()]]], [[], []]
-)
-for i in range(1, 6):
-    station[1][i] = station[1][i].enable_foundation(9)
+station = h_merge([[[cns], [default]], semitraversable.demo_1(5, 7)[5:], [[cns], [default]]], [[], []])
 
 # Road Stops
-overpass = overpass.lower_tile()
-road = road_ground_vanilla_layout.lower_tile()
+overpass = overpass
+road = road_ground_vanilla_layout
 roadstops = [[road] + [overpass] * 5 + [road]]
 
 # Objects
-center_ground = west_plaza_center.lower_tile()
+center_ground = west_plaza_center
 west_square = [[center_ground] * 7] * 2
 
 
 west_plaza = Demo(
-    station + roadstops + west_square, "West plaza", remap=get_1cc_remap(CompanyColour.WHITE), merge_bbox=True
+    station + roadstops + west_square,
+    "West plaza",
+    remap=get_1cc_remap(CompanyColour.WHITE),
+    merge_bbox=True,
+    altitude=[[1] * 8] * 2 + [[0] * 8] * 4,
 )
