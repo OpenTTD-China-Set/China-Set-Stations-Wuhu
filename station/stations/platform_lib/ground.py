f1f38642768871771e7107fa70a7207403809232
Yi Yang @ Asterodeia <ahyangyi@gmail.com>
2026-01-11 00:54:24 +0800

    ..

--- station/stations/platform_lib/ground.py
+++ station/stations/platform_lib/ground.py
@@ -75,6 +75,7 @@ def merge_ground(object_sprite, elevation=-1):
                     False,
                     -8 - 8 * elevation,
                     extended=True,
+                    nw_clip=(i % 3 == 0),
                     ne_clip=(i // 3 % 3 == 0),
                     sw_shareground=sw,
                     se_shareground=se,
